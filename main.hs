019f8f92efb8cd13ec909e42fee7727c089a3b17
洪識超 <shihchaohung@10-248-213-187.wireless.oregonstate.edu>
2020-02-19 16:11:03 -0800

    Create Int Bool data type

---
+++ main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Data.List
+
+data Expi = GetInt
+          | Lit Int
+          | Add Expi Expi
+          | Mul Expi Expi
+          | Mis Expi Expi
+          | Div Expi 
+
+data Expb = GetBool
+          | Bl Bool
+          | Bli Expi Expi
+          | Blb Expb Expb
+
