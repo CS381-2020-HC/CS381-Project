604b35cc809ec4b40d31e2f2308c00cb26e1c987
洪識超 <shihchaohung@10-248-213-187.wireless.oregonstate.edu>
2020-02-20 16:40:40 -0800

    Chenge the type of Int and Double

--- main.hs
+++ main.hs
@@ -5,9 +5,10 @@ import Data.List
 type Prog = [Cmd]
 data Cmd = Ifelse Expb Expi Expi
          deriving (Eq, Show)
-
+type No = Either Int Double
+type LeftRight = (No, No)
 data Expi = Get
-          | Lit Int
+          | Lit No
           | Add Expi Expi
           | Mul Expi Expi
           | Mis Expi Expi
@@ -27,20 +28,38 @@ data Expb = GetBool
           deriving (Eq, Show)
 
 test :: Expi
-test = Add (Lit 2) (Mul (Lit 6)(Lit 3))
+test = Add (Lit (Left 2)) (Mul (Lit (Left 6))(Lit (Left 3)))
 
 test1 :: Expb
-test1 = Bli_b (Add (Lit 2) (Mul (Lit 6)(Lit 3))) (Lit 21)
+test1 = Bli_s (Add (Lit (Left 2)) (Mul (Lit (Left 6))(Lit (Left 3)))) (Lit (Left 20))
 
-doInt :: Expi -> Int -> Int
+doleftright :: LeftRight -> String -> No
+doleftright (Left a, Left b) "+" = Left (a + b) 
+doleftright (Right a, Right b) "+" = Right (a + b)
+doleftright (Left a, Right b) "+" = Right ((fromIntegral a) + b)
+doleftright (Right a, Left b) "+" = Right (a + (fromIntegral b))
+doleftright (Left a, Left b) "-" = Left (a - b) 
+doleftright (Right a, Right b) "-" = Right (a - b)
+doleftright (Left a, Right b) "-" = Right ((fromIntegral a) - b)
+doleftright (Right a, Left b) "-" = Right (a - (fromIntegral b))
+doleftright (Left a, Left b) "*" = Left (a * b) 
+doleftright (Right a, Right b) "*" = Right (a * b)
+doleftright (Left a, Right b) "*" = Right ((fromIntegral a) * b)
+doleftright (Right a, Left b) "*" = Right (a * (fromIntegral b))
+doleftright (Left a, Left b) "/" = Left (a `div` b) 
+doleftright (Right a, Right b) "/" = Right (a / b)
+doleftright (Left a, Right b) "/" = Right ((fromIntegral a) / b)
+doleftright (Right a, Left b) "/" = Right (a / (fromIntegral b))
+
+doInt :: Expi -> No -> No
 doInt Get s = s
 doInt (Lit a) s =  a
-doInt (Add a b) s = (doInt a s) + (doInt b s)
-doInt (Mul a b) s = (doInt a s) * (doInt b s)
-doInt (Mis a b) s = (doInt a s) - (doInt b s)
-doInt (Div a b) s = (doInt a s) `div` (doInt b s)
+doInt (Add a b) s = doleftright ((doInt a s), (doInt b s)) "+"        
+doInt (Mul a b) s = doleftright ((doInt a s), (doInt b s)) "*"       
+doInt (Mis a b) s = doleftright ((doInt a s), (doInt b s)) "-"       
+doInt (Div a b) s = doleftright ((doInt a s), (doInt b s)) "/"       
 
-doBool :: Expb -> Int -> Bool
+doBool :: Expb -> No -> Bool
 doBool (Bl a) s = a
 doBool (Bli_s a b) s = (doInt a s) < (doInt b s)
 doBool (Bli_b a b) s = (doInt a s) > (doInt b s)
@@ -51,9 +70,9 @@ doBool (Bli_bq a b) s = (doInt a s) >= (doInt b s)
 doBool (Blb_q a b) s = (doBool a s) == (doBool b s)
 doBool (Blb_nq a b) s = (doBool a s) /= (doBool b s)
 
-doCmd :: Cmd -> Int -> Int 
+doCmd :: Cmd -> No -> No 
 doCmd (Ifelse a b c) s = if (doBool a s) then doInt b s else doInt c s
 
-doProg :: Prog -> Int -> Int
+doProg :: Prog -> No -> No
 doProg [] s = s
 doProg (x:xs) s = doProg xs (doCmd x s)
