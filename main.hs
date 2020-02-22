229ec3fe753ef38b808b686038ef982078aafc38
洪識超 <shihchaohung@10-248-213-187.wireless.oregonstate.edu>
2020-02-21 17:11:52 -0800

    Finish for function

--- main.hs
+++ main.hs
@@ -6,7 +6,9 @@ type Prog = [Cmd]
 type No = Either Int Double
 type LeftRight = (No, No)
 
-data Cmd = Ifelse Expb Expi Expi
+data Cmd = Ifelse Expb Prog Prog
+         | For No Expb No Prog
+         | Operation Expi
          deriving (Eq, Show)
 
 data Expi = Get
@@ -64,18 +66,54 @@ do_operation (Mis a b) s = do_operation_IntandDouble ((do_operation a s), (do_op
 do_operation (Div a b) s = do_operation_IntandDouble ((do_operation a s), (do_operation b s)) Divide      
 
 do_Bool :: Expb -> No -> Bool
-do_Bool (Bl a) s = a
+--do_Bool (Bl a) s = a
 do_Bool (Bli_s a b) s = (do_operation a s) < (do_operation b s)
 do_Bool (Bli_b a b) s = (do_operation a s) > (do_operation b s)
 do_Bool (Bli_q a b) s = (do_operation a s) == (do_operation b s)
 do_Bool (Bli_nq a b) s = (do_operation a s) /= (do_operation b s)
 do_Bool (Bli_sq a b) s = (do_operation a s) <= (do_operation b s)
 do_Bool (Bli_bq a b) s = (do_operation a s) >= (do_operation b s)
-do_Bool (Blb_q a b) s = (do_Bool a s) == (do_Bool b s)
-do_Bool (Blb_nq a b) s = (do_Bool a s) /= (do_Bool b s)
+--do_Bool (Blb_q a b) s = (do_Bool a s) == (do_Bool b s)
+--do_Bool (Blb_nq a b) s = (do_Bool a s) /= (do_Bool b s)
+
+test2 :: Prog
+test2 = [For (Left 0) (Bli_s (Lit (Left 0)) (Lit (Left 10))) (Left 1) [ Operation (Add Get (Lit (Left (-1)))) ] ]
 
 doCmd :: Cmd -> No -> No 
-doCmd (Ifelse a b c) s = if (do_Bool a s) then do_operation b s else do_operation c s
+doCmd (Ifelse a b c) s = if (do_Bool a s) then doProg b s 
+                         else doProg c s
+doCmd (Operation a) s = do_operation a s
+doCmd (For a b c d) s = if (do_Bool b s) then case b of Bli_s i j -> let 
+                                                                        result = (doProg d s) 
+                                                                        add = do_operation_IntandDouble (a, c) Plus
+                                                                     in 
+                                                                        doCmd (For add (Bli_s (Lit add) j) c d) result
+                                                        Bli_q i j -> let 
+                                                                        result = (doProg d s) 
+                                                                        add = do_operation_IntandDouble (a, c) Plus
+                                                                     in 
+                                                                        doCmd (For add (Bli_q (Lit add) j) c d) result
+                                                        Bli_nq i j -> let 
+                                                                        result = (doProg d s) 
+                                                                        add = do_operation_IntandDouble (a, c) Plus
+                                                                     in 
+                                                                        doCmd (For add (Bli_nq (Lit add) j) c d) result
+                                                        Bli_b i j -> let 
+                                                                        result = (doProg d s) 
+                                                                        add = do_operation_IntandDouble (a, c) Plus
+                                                                     in 
+                                                                        doCmd (For add (Bli_b (Lit add) j) c d) result
+                                                        Bli_sq i j -> let 
+                                                                        result = (doProg d s) 
+                                                                        add = do_operation_IntandDouble (a, c) Plus
+                                                                      in 
+                                                                        doCmd (For add (Bli_sq (Lit add) j) c d) result
+                                                        Bli_bq i j -> let 
+                                                                        result = (doProg d s) 
+                                                                        add = do_operation_IntandDouble (a, c) Plus
+                                                                      in 
+                                                                        doCmd (For add (Bli_bq (Lit add) j) c d) result
+                        else s
 
 doProg :: Prog -> No -> No
 doProg [] s = s
